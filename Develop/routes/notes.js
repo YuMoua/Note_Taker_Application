039f0fb9d951b4903819fd3b2f7f2a6cecc67511
unknown <youyu00022@gmail.com>
2024-04-16 22:11:44 -0500

    updated

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,25 +1,51 @@
 const notes = require('express').Router();
 const notesData = require('../db/notes.json')
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils')
 
 notes.get('/', (req, res) => {
-    res.json(notesData)
-  });
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
 
 
 notes.post('/', (req, res) => {
-    console.log(req.body);
-  
-    // const { title, text } = req.body;
-  
-    // if (req.body) {
-    //   const newNote = {
-    //     title,
-    //     text
-    //   };
+  console.log(req.body);
 
-      newNote = req.body;
-      newNote.id = Math.floor((1 + Math.random())*0x10000).subString(1);
-      console.log(newNote)
-  });
 
-  module.exports = notes;
+  newNote = req.body;
+  newNote.id = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  console.log(newNote);
+  readAndAppend(newNote, './db/notes.json')
+
+
+  const response = {
+    status: 'success',
+    body: newNote,
+  };
+
+  res.json(response)
+});
+
+
+notes.delete('/:id', (req, res) => {
+  console.log(req.body);
+  const noteId = req.params.id
+  readFromFile('./db/notes.json', 'utf8')
+    .then((data) => {
+      const parseData = JSON.parse(data)
+      const newData = parseData.filter((note) => note.id !== noteId)
+      // const parseData = JSON.parse(newData)
+      console.log(newData)
+      return newData
+    })
+    .then((filteredData) => {
+      writeToFile('./db/notes.json', filteredData)
+    })
+    .then((filteredData) => {
+      res.json(filteredData)
+    })
+})
+
+
+
+
+module.exports = notes;
